ba00dbfcdd426c2b5db67132b45dc356f0d2c259
darkvsp <64093118+darkvsp@users.noreply.github.com>
2020-09-22 11:40:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ echo "CALLER setup started"
 chmod +x caller.sh
 mkdir contact
 mv list.txt $HOME/caller/contact
+mv caller.sh /data/data/com.termux/files/usr
+cd /data/data/com.termux/files/usr/
+mv caller.sh bin
 echo "*processing.....!"
 sleep 1.5
 echo "setup completed, to run the tool use ./caller.sh"
